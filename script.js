d206520f9530fd8119240dd233ab34607a5d1fdd
syedahalam <syedahalam@yahoo.com>
2021-11-04 21:54:27 -0400

    day1

---
+++ script.js
@@ -0,0 +1,14 @@
+let mmasss = 78;
+let mheight = 1.69;
+
+let jmasss =92;
+let jheight = 1.95;
+
+let mbmi = (mmasss/mheight ** 2)
+console.log(mbmi)
+
+let jbmi = jmasss / jheight ** 2;
+console.log(jbmi);
+
+let ismarkmbihigher = mbmi >jbmi
+console.log(ismarkmbihigher)
